eeea3248e5006a8325102ce566591b152d6fde78
Priyanshu Saini <priyanshusaini1102@gmail.com>
2023-04-10 18:42:11 +0530

    add key to product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import productImg from '../../public/img/products/clothing-store-app.png'
 import Link from 'next/link'
 
-const ProductCard = ({product}) => {
+const ProductCard = ({key,product}) => {
 
   return (
-    <Link href={`./product/${product?._id}`} > 
+    <Link key={key} href={`./product/${product?._id}`} > 
     <div className='shadow-md border text-white bg-white w-60 rounded' >
         <Image className='rounded' src={productImg} alt='Product Image'  />
         <div className='text-gray-700 font-sans p-3 font-semibold text-center' >
