2be631c59f4f454c15db89c5af90517aca652594
Priyanshu Saini <priyanshusaini1102@gmail.com>
2023-06-06 16:46:49 +0530

    add category feature and whatsapp feature demo

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
       <div className='flex flex-col items-center justify-center' >
         <p className='font-sans font-semibold text-7xl' style={{'textShadow':'0 0 10px #764BD1'}}> Nexstores </p>
         <p className='text-sm font-semibold text-gray-400 ml-5' >Transforming your offline store into an online success</p>
-        <p className='m-4 flex items-center space-x-2' > <span> Call Now:</span> <span className='p-3 shadow-inner shadow-indigo-50 rounded-xl' >+91 9697989493</span> </p>
+        <p className='m-4 flex items-center space-x-2' > <span> Call Now:</span> <a href='https://wa.me/7017413590' target='blank' className='p-3 shadow-inner shadow-indigo-50 rounded-xl' >+91 9697989493</a> </p>
       </div>
         <Image height={400} src={bannerImg} alt="banner cannot load" priority />
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,18 +6,76 @@ import Banner from '@/components/Banner'
 import ProductCard from '@/components/ProductCard'
 import Link from 'next/link'
 import { createClient } from "next-sanity";
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useRef, useState } from 'react'
+import { ChevronDownIcon, EditActiveIcon, EditInactiveIcon } from '@heroicons/react/20/solid'
 
 const inter = Inter({ subsets: ['latin'] })
+const categories = [
+  'All',
+  'Website',
+  'App'
+]
+
 
 export default function Home({products}) {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  useEffect(()=>{
+    const filtered=products.filter((product)=> product.type.includes(selectedCategory==='All' ? '' : selectedCategory));
+    setFilteredProducts(filtered);
+  },[selectedCategory,products])
   return (
     <>
       <Banner />
+      <div className='max-w-6xl p-4 border mx-auto bg-white rounded-lg shadow-violet-500 shadow-xl'>
+      <div className='mb-2'>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-violet-500 bg-opacity-20 px-4 py-2 text-sm font-medium text-violet-500 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+            {selectedCategory}
+            <ChevronDownIcon
+              className="ml-2 -mr-1 h-5 w-5 text-violet-500 hover:text-violet-600"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute z-10 left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            {categories.map((c)=>(<div key={c} className="px-1 py-1 ">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    onClick={()=>setSelectedCategory(c)}
+                    className={`${
+                      active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    {c}
+                  </button>
+                )}
+              </Menu.Item>
+            </div>))}
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+
       
-      <div className='p-4 flex flex-wrap md:justify-around justify-center items-center space-x-2 space-y-4 max-w-6xl mx-auto bg-white rounded-lg shadow-violet-500 shadow-xl' >
+      <div className='flex flex-wrap md:justify-around justify-center items-center space-x-2 space-y-4' >
       {
-        products?.map((product)=> <Link key={product?.id} href={`./product/${product?.id}`}><ProductCard key={product?.id} product={product}/></Link>)
+        filteredProducts?.map((product)=> <Link key={product?.id} href={`./product/${product?.id}`}><ProductCard key={product?.id} product={product}/></Link>)
       }
+      </div>
       </div>
 
     </>
